ec1a1b2848e3489c384cbc32cdc1dd2c1a1aa592
omeryurttutan <talhayurttutan@gmail.com>
2023-07-21 17:12:45 +0300

    update ball movement and reflect

--- Assets/Scripts/GamePlay/Ball.cs
+++ Assets/Scripts/GamePlay/Ball.cs
@@ -1,47 +1,35 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.UIElements;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
+
 public class Ball : MonoBehaviour
 {
-   public float speed = 10f;
-   public new Rigidbody rb { get; private set; }
+    public float speed = 1f;
+    private void Update()
+    {
+        var nextPos = transform.forward * (speed * Time.deltaTime);
+        
+        var ray = new Ray(transform.position, nextPos.normalized);
+
+        if (Physics.SphereCast(ray,0.1f,out RaycastHit hit,nextPos.magnitude))
+        {
+            var NewPos = Vector3.Reflect(transform.forward, hit.normal);
+            transform.forward = NewPos;
+        }
+        else
+        {
+            transform.position += nextPos;
+       
+        }
+       
 
 
-   private void Awake()
-   {
-      rb = GetComponent<Rigidbody>();
-   }
+    }
 
-   void Start()
-   {
-     ResetBall();
-
-   }
-   
-   public void ResetBall()
-   {
-      rb.velocity = Vector3.zero;
-      transform.position = Vector3.zero;
-
-      Invoke(nameof(SetRandomTrajectory), 1f);
-   }
-
-   private void FixedUpdate()
-   {
-      rb.velocity = rb.velocity.normalized * speed;
-   }
-
-
-   private void SetRandomTrajectory()
-   {
-      Vector3 force=Vector3.zero;
-      force.x = Random.Range(-1f, 1f);
-      force.y = -1f;
-      
-      this.rb.AddForce(force* speed, ForceMode.Impulse);
-      
-   }
+    
 }
